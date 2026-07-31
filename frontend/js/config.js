18151564565d7bb11fb1d6fe8546475ebfa4b57d
Wesley <wesleysantoscardoso2404@gmail.com>
2026-07-31 14:32:23 -0300

    alterando dominio

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,5 @@
 window.CONFIG = {
-    //API_URL: "https://logs.muvysystem.com.br"
-    API_URL: "http://localhost:9000"
+    API_URL: "https://logs.muvysystem.com.br"
 };
 
 window.API = window.CONFIG.API_URL;
